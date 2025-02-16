3764ef8a1a960dc511a73e5345fce4d9418919a6
mohdswar9@gmail.com <mohdswar9@gmail.com>
2025-02-16 13:37:30 +0300

    add some shit

---
+++ app.js
@@ -0,0 +1,31 @@
+// const array1 = ['vanilla', 'chocolate', 'strawberry'];
+
+// const array2 = array1.map((currentElement) => {
+//   return currentElement + ' ice cream';
+// });
+
+// console.log(array2); // ['vanilla ice cream', 'chocolate ice cream', 'strawberry ice cream']
+
+
+// Use `.map()` to iterate over the following array:
+
+const nums = [13, 87, 2, 89, 12, 4, 90, 63];
+
+// Create a new array where each value is multiplied by 2 and log the new array.
+const nums2 = nums.map((currentElement) => {
+  return currentElement * 2;
+
+});
+
+console.log(nums2)
+
+
+
+// Given the following array, use destructuring to pull out the first and second values and place them into variables. Log both variables.
+
+const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
+
+// Your code here
+const arr = [food1, food2] = pizzaToppings
+console.log(food1);
+console.log(food2);
